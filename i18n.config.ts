09d3eff497338d73dd22eb4c39c7003d5222d27e
Supricom <supricom@MacBook-Air-de-Supricom.local>
2026-08-18 09:51:55 -0400

    fix: disenador

--- i18n.config.ts
+++ i18n.config.ts
@@ -4,6 +4,7 @@ import { defineRouting } from "next-intl/routing";
 export const routing = defineRouting({
   locales: ["es", "en"],
   defaultLocale: "es",
+  
 });
 
 export type Locale = (typeof routing.locales)[number];
